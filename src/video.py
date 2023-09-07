3309c6d23f73d64ced490452a2ec49ec63c562ef
AleksandrZaec <zaecam95@mail.ru>
2023-09-07 21:03:48 +0300

    Создал файл `src/video` и в нем класс `Video` Реализовал инициализацию реальными данными следующих атрибутов экземпляра класса `Video`:   - id видео   - название видео   - ссылка на видео   - количество просмотров   - количество лайков
    Создал второй класс для видео `PLVideo`, который инициализируется  'id видео' и 'id плейлиста'
    
    Реализовал инициализацию реальными данными следующих атрибутов экземпляра класса `PLVideo`:
      - id видео
      - название видео
      - ссылка на видео
      - количество просмотров
      - количество лайков
      - id плейлиста
    
    Реализовал класс `PlayList`, который инициализируется _id_ плейлиста и имеет следующие публичные атрибуты:
      - название плейлиста
      - ссылку на плейлист
    
    Реализовал следующие методы класса `PlayList`
      - `total_duration` возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста (обращение как к свойству, использовать `@property`)
      - `show_best_video()` возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)

---
+++ src/video.py
@@ -0,0 +1,59 @@
+from googleapiclient.discovery import build
+import os
+
+
+class Video():
+
+    """ Класс видео """
+
+    def __init__(self, video_id):
+
+        """ Инициализация реальными данными атрибутов """
+
+        self.video_id = video_id
+
+        try:
+            self.dict_of_video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            self.title = str(self.dict_of_video['items'][0]['snippet']['title'])
+            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.count_views = int(self.dict_of_video['items'][0]['statistics']['viewCount'])
+            self.like_count = int(self.dict_of_video['items'][0]['statistics']['likeCount'])
+        except IndexError:
+            self.title = None
+            self.count_views = None
+            self.like_count = None
+            self.video_url = f"https://www.youtube.com/watch?v={self.video_id}"
+
+
+    @classmethod
+    def get_service(cls):
+
+        """ Возвращает объект для работы с YouTube API """
+
+        return build("youtube", "v3", developerKey=os.getenv("YT_API_KEY"))
+
+
+    def __str__(self):
+
+        """ Метод, который выводит название видео """
+
+        return self.title
+
+
+class PLVideo(Video):
+
+    """ класс для видео, который инициализируется 'id видео' и 'id плейлиста """
+
+    def __init__(self, video_id, playlist_id):
+
+        """ Инициализация реальными данными атрибутов  """
+
+        super().__init__(video_id)
+
+        self.dict_of_video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+
+        self.video_title = str(self.dict_of_video['items'][0]['snippet']['title'])
+        self.video_url = f"https://youtu.be/gaoc9MPZ4bw/{self.video_id}"
+        self.count_views = int(self.dict_of_video['items'][0]['statistics']['viewCount'])
+        self.count_likes = int(self.dict_of_video['items'][0]['statistics']['likeCount'])
+        self.playlist_id = playlist_id
